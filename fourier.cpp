9c05103dcfd99da2bfd560d325816227bd6cf1ef
deanturpin <git@brightcoin.uk>
2018-06-03 11:19:09 +0100

    Fork from repo spectrum_analyser

---
+++ fourier.cpp
@@ -0,0 +1,44 @@
+#include "fourier.h"
+#include <algorithm>
+#include <complex>
+#include <iostream>
+#include <vector>
+
+std::vector<std::complex<double>> twiddle;
+
+void fourier_init() {
+
+  using namespace std;
+
+  const auto bins = fourier_bins;
+  twiddle.reserve(bins * bins);
+
+  // Populate twiddle matrix - Euler's magic
+  for (unsigned int k = 0; k < bins / 2; ++k)
+    for (unsigned int n = 0; n < bins; ++n)
+      twiddle.push_back(exp(2i * M_PI * double(k) * double(n) / double(bins)));
+}
+
+std::vector<double> fourier(const std::vector<short> &samples) {
+
+  using namespace std;
+
+  const auto bins = fourier_bins;
+
+  // The Fourier transform is the dot product of the twiddle matrix and the
+  // original samples. Only run over the first half of the matrix as the other
+  // half is a mirror image.
+  vector<double> fou(bins / 2);
+  generate(
+      fou.begin(), fou.end(), [&samples, &bins, k = 0ul ]() mutable {
+
+        complex<double> sum;
+        for (unsigned int n = 0; n < bins; ++n)
+          sum += twiddle[(k * bins) + n] * complex<double>(samples.at(n), 0);
+
+        ++k;
+        return abs(sum);
+      });
+
+  return fou;
+}
---
+++ makefile
@@ -0,0 +1,18 @@
+CXX=g++-6
+CCFLAGS = -std=c++14 --all-warnings --extra-warnings -pedantic-errors \
+	 -Wshadow -Wfloat-equal -Weffc++ -Wdelete-non-virtual-dtor
+DEBUG = -g --coverage
+
+%.o: %.cpp
+	$(CXX) -o $@ $< $(CCFLAGS) $(DEBUG) -c
+
+all: spectrum
+
+spectrum: spectrum.o fourier.o
+	$(CXX) -o $@ $^ $(DEBUG)
+
+clean:
+	rm -f *.o spectrum
+
+noise: spectrum
+	arecord -q -f S16_LE -c1 -r 8000 | ./spectrum
