deaef327f0a026f3f4f4d3fcced519cfc91c544c
deanturpin <git@brightcoin.uk>
2018-06-10 00:02:06 +0100

    Replace underscore in title with spaces - closes #15

--- create_gnuplot_config.sh
+++ create_gnuplot_config.sh
@@ -6,7 +6,7 @@ readonly name=$1
 [[ $name =~ zoom ]] && font_size=10 || font_size=3
 
 echo set terminal svg size 1500,900
-echo set title \"$name\"
+echo set title \"${name//_/ }\"
 echo set output \"$name.svg\"
 echo set format y \"\"
 echo set xtics 10
