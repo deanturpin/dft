8d764f40051bb1a6857f4ec2418b5107a5465a3d
deanturpin <git@brightcoin.uk>
2018-06-09 10:41:22 +0100

    Trim toot and simplify headings

--- create_readme.sh
+++ create_readme.sh
@@ -7,18 +7,21 @@ echo
 # Create a section for each WAV
 for svg in *.svg; do
 
-  # Reconstruct WAV
-  title=${svg%.*}
-  wav=wav/$title.wav
-  wav=${wav/_zoom/}
-  wav=${wav/_full/}
-  markdown=${wav%.*}.md
+  # Prepare SVG file name to convert into other file name
+  stem=${svg%.*}
+  stem=${stem/_zoom/}
+  stem=${stem/_full/}
+
+  # Construct file names
+  wav=wav/$stem.wav
+  markdown=wav/$stem.md
+  title=${stem//_/ }
 
   # Print info for the first graph if there is any
-  if [[ $title =~ full ]]; then
+  if [[ $svg =~ full ]]; then
 
     # Print section header
-    echo "# ${title//_/ }"
+    echo "# $title"
 
     [[ -e $markdown ]] && cat $markdown
     echo "Listen to the [audio]($wav)."
