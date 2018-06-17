237f0979d59acb01f09611cd264ce3eeef75c3e3
deanturpin <git@brightcoin.uk>
2018-06-17 18:55:04 +0100

    Reword

--- dft.h
+++ dft.h
@@ -9,14 +9,14 @@
 // DFT is a header-only discrete Fourier transform implementation written in
 // C++14. Libraries often use optimisations that restrict dimensions of the
 // sample array (power of two) but without these limitations we can explore the
-// beauty of the algorithm and apply it to problems where we couldn't use a
-// "fast" implementation. It was initially written to study the characteristic
-// spectral response of my various instruments. The calculate() routine takes a
-// pair of STL container iterators and returns the Fourier transform as a vector
-// of bins. We could consider parallelising the matrix calculation but I've
-// elected to keep the library simple (single-threaded) and delegated the
-// multi-core utilisation to the makefile: the build process generates multiple
-// images in parallel.
+// the algorithm and apply where we couldn't use a "fast" implementation. It was
+// originally written to study the characteristic spectral response of my
+// various musical instruments but can be applied in other domains. The
+// calculate() routine takes a pair of STL container iterators and returns the
+// Fourier transform as a vector of bins. We could consider parallelising the
+// matrix calculation but I've elected to keep the library simple
+// (single-threaded) and delegated the multi-core utilisation to the makefile:
+// the build process generates multiple images in parallel.
 //
 // https://en.wikipedia.org/wiki/Discrete_Fourier_transform
 // https://jackschaedler.github.io/circles-sines-signals
