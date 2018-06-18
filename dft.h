fbd490faf10117143f57d75732699b89322b47ab
deanturpin <git@brightcoin.uk>
2018-06-18 11:18:18 +0100

    Rename bins to total samples

--- dft.h
+++ dft.h
@@ -9,9 +9,9 @@
 // DFT is a header-only discrete Fourier transform implementation written in
 // C++14. Libraries often use optimisations that restrict dimensions of the
 // sample array (power of two) but without these limitations we can explore the
-// the algorithm and apply where we couldn't use a "fast" implementation. It was
+// algorithm and apply it where we couldn't use a "fast" implementation. It was
 // originally written to study the characteristic spectral response of my
-// various musical instruments but can be applied in other domains. The
+// various musical instruments but it can be applied in other domains. The
 // calculate() routine takes a pair of STL container iterators and returns the
 // Fourier transform as a vector of bins. We could consider parallelising the
 // matrix calculation but I've elected to keep the library simple
@@ -31,13 +31,13 @@ template <typename Iterator> auto calculate(Iterator begin, Iterator end) {
   // For each Fourier bin we need to iterate over each sample - O(n^2) - but we
   // will return only half as many bins as samples - the upper half is a mirror
   // image of the lower, i.e., redundant.
-  const double bins = std::distance(begin, end);
+  const double total_samples = std::distance(begin, end);
 
   // The "twiddle matrix" is usually generated up front but as we're performing
   // a one-shot calculation it can be refactored into a single loop. Normally
   // you would expect to see integer array indices but here a floating-point
   // counter is used to avoid a cast in the subsequent calculation.
-  for (double k = 0.0; k < bins / 2; ++k) {
+  for (double k = 0.0; k < total_samples / 2; ++k) {
 
     // Iterate over all samples for the current bin index (k), calculate the
     // response and store the result. Note the sample index (n) is incremented
@@ -45,10 +45,10 @@ template <typename Iterator> auto calculate(Iterator begin, Iterator end) {
     // the algorithm.
     std::vector<std::complex<double>> fou;
     std::transform(begin, end, std::back_inserter(fou),
-                   [ n = 0.0, &bins, &k ](const auto &sample) mutable {
+                   [ n = 0.0, &total_samples, &k ](const auto &sample) mutable {
                      return exp(std::complex<double>{0.0, 2.0} *
                                 3.14159265358979323846264338328 * k * n++ /
-                                bins) *
+                                total_samples) *
                             double(sample);
                    });
 
@@ -56,7 +56,7 @@ template <typename Iterator> auto calculate(Iterator begin, Iterator end) {
     // it by the window size (number of samples).
     dft.push_back(std::abs(std::accumulate(std::cbegin(fou), std::cend(fou),
                                            std::complex<double>{}) /
-                           bins));
+                           total_samples));
   }
 
   return dft;
